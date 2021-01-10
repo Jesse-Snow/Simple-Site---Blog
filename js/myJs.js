2367831e2105de05dbe806ce9aaf40f8d06791f9
Jesse Snow <jesse.neves@outlook.com>
2021-01-10 12:44:36 -0300

    Add border in image witch JavaScript

---
+++ js/myJs.js
@@ -0,0 +1,3 @@
+let hBig = document.getElementById("imagem")
+hBig.addEventListener("mouseover", () => hBig.style.border="5px solid black")
+hBig.addEventListener("mouseout", () => hBig.style.border = "none")
